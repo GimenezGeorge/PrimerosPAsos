af4cc72d175592537a6f8592e85c2fffeb440591
GimenezGeorge <jorge.2015.8893@gmail.com>
2020-07-28 19:07:46 -0300

    Más funciones

--- preparen.py
+++ preparen.py
@@ -2,4 +2,13 @@ class Producto:
 	def __init__(self, nombre, precio):
 		self.__nombre = nombre
 		self.__precio = precio
-		
+		self.__primera_necesidad = primera_necesidad
+
+	def get_nombre(self):
+		return self.__nombre
+
+	def get_primera_necesidad(self):
+		return self.__primera_necesidad
+
+	def get_precio(self):
+		return self.__precio
